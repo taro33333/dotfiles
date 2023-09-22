929344cafe9fc80b9a45c808e51e36d302c11dee
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-09-22 23:07:45 +0900

    Add script to install asdf package

---
+++ modules/scripts/asdf_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# asdfがなければインストール
+if [ ! -e "$HOME/.asdf" ]; then
+  echo "installing asdf..."
+  git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch v0.12.0
+  exec $SHELL -l
+fi
+
+PLUGS=$(
+  cat <<EOF
+golang
+nodejs
+pnpm
+kubectl
+kind
+awscli
+jq
+EOF
+)
+
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin-add pnpm
+asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
+asdf plugin-add kind https://github.com/reegnz/asdf-kind.git
+asdf plugin add awscli
+asdf plugin-add jq https://github.com/lsanwick/asdf-jq.git
+
+echo "$PLUGS" | xargs -L 1 -I{} asdf install {} latest
+echo "$PLUGS" | xargs -L 1 -I{} asdf global {} latest
