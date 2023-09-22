d32f5077452eac5ea0a18b12d1b448fcb054de48
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-09-22 23:08:41 +0900

    Add script to install node package

---
+++ modules/scripts/node_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Node npm packages
+NPM_PACKAGES=$(
+  cat <<EOF
+jest
+eslint
+prettier
+@google/clasp
+EOF
+)
+
+echo "install global packages for development"
+echo "$NPM_PACKAGES" | xargs -L 1 npm i --location=global
+
+if type "asdf" >/dev/null 2>&1; then
+  echo "reshim asdf nodejs" #コマンドが存在する時の処理
+  asdf reshim nodejs
+fi
