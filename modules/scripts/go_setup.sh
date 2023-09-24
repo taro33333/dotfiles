dc4306ddde80dec33f2ce63d128fe7627347b4fe
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-09-24 11:46:03 +0900

    add tbls pakcage

--- modules/scripts/go_setup.sh
+++ modules/scripts/go_setup.sh
@@ -16,6 +16,7 @@ github.com/mdempsky/gocode@latest
 google.golang.org/protobuf/cmd/protoc-gen-go@latest
 google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest
+github.com/k1LoW/tbls@latest
 EOF
 )
 
