39569b109bd14986b4e6ce6006950c52116ce2ec
Ryotaro <70475997+RyotaroSeto@users.noreply.github.com>
2024-06-08 12:48:34 +0900

    Update alias.sh

--- modules/rc/alias.sh
+++ modules/rc/alias.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 alias lg="lazygit"
-alias vim="nvim"
 alias d="docker"
 alias dc="docker compose"
+alias dcu=‘docker compose up -d’
+alias dce=‘docker compose exec’
+alias dokcer="docker"
 alias k="kubectl"
+alias la=‘ls -la’
+alias ll=‘ls -la’
+alias gti=‘git’
+alias igt=‘git’
