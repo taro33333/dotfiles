451132a298348cc8aa1273c7bbfe7756a91c3895
Ryotaro <70475997+RyotaroSeto@users.noreply.github.com>
2024-07-18 22:18:06 +0900

    Update mac_packages_install.sh

--- modules/scripts/mac_packages_install.sh
+++ modules/scripts/mac_packages_install.sh
@@ -10,7 +10,7 @@ if ! (type "brew" >/dev/null 2>&1); then
   fi
 fi
 
-PACKAGES="bash zsh git gpg openssl curl hashicorp/tap/vault ripgrep opa trivy"
+PACKAGES="bash zsh git gpg openssl curl hashicorp/tap/vault ripgrep opa trivy hadolint"
 GUI_APPS="warp raycast"
 
 # パッケージマネージャーのセットアップ
